f38d21fb609f86ad5bc800d1ecd37f8165233c97
lovegreenearth <green.soft361@gmail.com>
2023-09-20 07:53:28 -0500

    server and api url config

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('static'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
 
